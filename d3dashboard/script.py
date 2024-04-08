8f5c2b7ca71c47ec0c2ec47f30c94a456aeb5975
donpuz <mrdonpuz@gmail.com>
2024-04-08 16:40:13 -0400

    add basic tooltip

--- d3dashboard/script.py
+++ d3dashboard/script.py
@@ -1,6 +1,9 @@
 import json
 import sys
 import pandas as pd
+import random
+import string
+import numpy as np
 
 def main():
     # sys.stdout.reconfigure(buffering=0)
@@ -16,7 +19,14 @@ def main():
     
     url = "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/2_TwoNum.csv"
     data = pd.read_csv(url)
+
+    def random_string(length=5):
+        return ''.join(random.choice(string.ascii_letters) for _ in range(length))
+    
+    data['score'] = np.random.rand(data.shape[0])
+    data['caption'] = [random_string() for _ in range(data.shape[0])]
     # Print JSON data
+
     json_data = data.to_json(orient='records')
     print(json_data)
 
